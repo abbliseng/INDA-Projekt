aa3c2131a05cc8e1d641b3bbc618758023bb0d93
Nils Malmberg <nils@u1.se>
2024-03-22 08:50:16 +0100

    adding database stuff

---
+++ database.sh
@@ -0,0 +1,9 @@
+docker run --name ddagen-db -p 5432:5432 \
+    -e POSTGRES_PASSWORD=dkm \
+    -e POSTGRES_DB=dkm \
+    -e POSTGRES_USER=dkm \
+    -v <path to persistence folder>:/var/lib/postgresql/data \
+    postgres
+
+
+# postgres://dkm:dkm@localhost:5432/dkm
