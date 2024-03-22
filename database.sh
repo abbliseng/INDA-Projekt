693f291d7009fedf802241c309c7ebf19c82a83d
Lisa <54662679+abbliseng@users.noreply.github.com>
2024-03-22 10:29:00 +0100

    init events

--- database.sh
+++ database.sh
@@ -2,7 +2,7 @@ sudo docker run --name ddagen-db -p 5432:5432 \
     -e POSTGRES_PASSWORD=dkm \
     -e POSTGRES_DB=dkm \
     -e POSTGRES_USER=dkm \
-    -v /mnt/dkm_database/:/var/lib/postgresql/data \
+    -v ./mnt/dkm_database/:/var/lib/postgresql/data \
     postgres
 
 sudo docker rm ddagen-db
