4b9608b1dc4e1fae8bfe27851a5b85473a7cb7a3
Lisa <54662679+abbliseng@users.noreply.github.com>
2024-04-26 11:45:59 +0200

    Remove arrows and dots when there is only one upcoming event

--- dkm/src/components/Carousel/BackgroundCarousel.jsx
+++ dkm/src/components/Carousel/BackgroundCarousel.jsx
@@ -37,27 +37,32 @@ const BackgroundCarousel = ({items}) => {
             <h1>{items[index].name}</h1>
             <p>{items[index].description}</p>
         </div>
-        <div class="overlay">
-            <button class="left" onClick={handlePrevious}>
-                <img src="./Icons/left-arrow.svg" alt="left"/>
-            </button>
-            <div class="section"
-                // FIXME: 
-                // onClick={() => window.location.href = `/events/${items[index].id}`}
-                onClick={() => window.location.href = `https://www.facebook.com/events/${items[index].id}`}
-            ></div>
-            <button class="right" onClick={handleNext}>
-                <img src="./Icons/right-arrow.svg" alt="right"/>
-            </button>
-        </div>
-        <div class="dots">
-            {items.map((item, i) => (
-                <button
-                    class={i === index ? "active" : ""}
-                    onClick={() => setIndex(i)}
-                ></button>
-            ))}
-        </div>
+        {
+          items.length > 1 &&
+          <>
+            <div class="overlay">
+                <button class="left" onClick={handlePrevious}>
+                    <img src="./Icons/left-arrow.svg" alt="left"/>
+                </button>
+                <div class="section"
+                    // FIXME: 
+                    // onClick={() => window.location.href = `/events/${items[index].id}`}
+                    onClick={() => window.location.href = `https://www.facebook.com/events/${items[index].id}`}
+                ></div>
+                <button class="right" onClick={handleNext}>
+                    <img src="./Icons/right-arrow.svg" alt="right"/>
+                </button>
+            </div>
+            <div class="dots">
+                {items.map((item, i) => (
+                    <button
+                        class={i === index ? "active" : ""}
+                        onClick={() => setIndex(i)}
+                    ></button>
+                ))}
+            </div>
+          </>
+        }
     </div>
   );
 };
