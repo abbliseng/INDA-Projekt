0e0174e3f992ee691666563b2dc435d569595dbf
Lisa <54662679+abbliseng@users.noreply.github.com>
2024-04-29 11:56:40 +0200

    Sync with main (#18)
    * adding new branch
    
    * updating footer
    
    * Updating footer
    
    * adding a menu thiny to the header
    
    * fixing a fun overflow bug 🐛
    
    * worm? 🪱🪱🪱🪱🪱🪱🪱🪱🪱🪱🪱
    
    * dockerized application
    
    * layout stuff
    
    * removing leftover file
    
    * Lazy fix to header
    
    * header lazy fix
    
    ---------
    
    Co-authored-by: Nils Malmberg <nils@u1.se>
    Co-authored-by: Nils Malmberg <69248971+ilmal@users.noreply.github.com>

---
+++ dkm/src/pages/Worm/Worm.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import "./worm.scss";
+
+const Worm = () => {
+    const rnd_drap = Math.floor(Math.random() * 100);
+
+    return (
+        <>
+            <div class="worm-container">
+                <div class="worm">
+                    <img src="/worm.gif" alt="🪱" />
+                </div>
+            </div>
+
+            <script>
+
+
+            </script>
+        </>
+    );
+    }
+
+export default Worm;
