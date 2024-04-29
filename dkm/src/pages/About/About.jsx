4384615d34c52d4a268a6140f8b9762ffd7a26a8
Lisa <54662679+abbliseng@users.noreply.github.com>
2024-04-29 15:13:37 +0200

    Add text content

--- dkm/src/pages/About/About.jsx
+++ dkm/src/pages/About/About.jsx
@@ -3,8 +3,36 @@ import './about.scss';
 
 const About = () => {
     return (
-        <div>
-        <h1>About</h1>
+        <div class="c">
+            <div class="d">
+                <h1>Vad gör DKM?</h1>
+                <div class="break"></div>
+                <h2>Pubar</h2>
+                <p>
+                    Varje onsdag anordnar vi pubar för alla på data- och mediasektionen i META!
+                    Detta är ett jätteroligt tillfälle att träffa folk på sektionen och bara mingla och dansa!
+                    <br/>
+                    Du som söker DKM behöver inte ha någon erfarenhet av att stå i bar tidigare. Under ditt första år kommer du som knatte få lära dig allt som behövs av oss mästare. Det viktigaste är att vi har kul tillsammans!
+                </p>
+                <div class="break"></div>
+                <h2>Sittningar</h2>
+                <p>
+                    Sittningar och gasquer är något vi tycker är väldigt kul! Utöver att vi i klubbmästeriet går på massa roliga gasquer varje år så anordnar vi även minst en egen sittning för datasektionen varje termin.
+                    Så om du tycker det verkar roligt att anordna gasquer och sittningar för sektionen är DKM helt för dig!
+                </p>
+                <div class="break"></div>
+                <h2>Interna event</h2>
+                <p>
+                    Att vara medlem i DKM innebär inte endast att hänga i META och fixa pubar och sittningar, något av det roligaste med att vara med i DKM är våra interna events och den starka gemenskapen som skapas under åren!!
+                    På våra internevent får man en chans att lära känna varandra ännu bättre och många event har vi också ed vår fina cliff (äldre DKMare) som är grymma på att festa och sapa fantastiska minnen! Vi åker även på en resa varje år med hela DKM!
+                </p>
+                <div class="break"></div>
+                <h2>Andra sektioner & klubbmästerier</h2>
+                <p>
+                    Utöver allt roligt som vi gör med sektionen har vi även samarbeten med andra sektioners klubbmästerier! Detta är ett perfekt sett att lära känna fler människor på KTH.
+                    Vi har framförallt mycket sammarbeten med MKM och CLW där vi bland annat anordnar en gemensam gasque varje år! Utöver det har vi föera interna event med dessa klubbmästerier.
+                </p>
+            </div>
         </div>
     );
 };
